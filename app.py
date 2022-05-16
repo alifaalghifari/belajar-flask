f1115977be90b3c5f66136a6b68caeb4fc900de1
Muh alif alghifari <alifalghifari798@gmail.com>
2022-05-16 17:58:29 +0800

    belajar url, cookie, session, dan flash

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import (Flask, flash, make_response,
+                   redirect, render_template, request, session, url_for
+                   )
 app = Flask(__name__)
+app.secret_key = "randomstring"
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    search = request.args.get('search')
+    return render_template('index.html', search=search)
 
 
 @app.route('/setting')
@@ -25,5 +29,33 @@ def show_blog(blog_id):
 @app.route('/login', methods=['GET', 'POST'])
 def show_login():
     if request.method == "POST":
-        return "Email kamu adalah " + request.form["email"]
+        # cookie
+        # resp = make_response("Email kamu adalah " + request.form["email"])
+        # resp.set_cookie('email_user', request.form["email"])
+
+        # sesion
+        session['username'] = request.form['email']
+
+        # flash message
+        flash("Kamu berhasil login!", "success")
+        return redirect(url_for('show_profile', username=session['username']))
+
+        # return resp
+
+    if 'username' in session:
+        username = session['username']
+        return redirect(url_for('show_profile', username=username))
+
     return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('show_login'))
+
+
+@app.route('/getcookie')
+def get_cookie():
+    email = request.cookies.get('email_user')
+    return "Email yang tersimpan di cookie adalah " + email
