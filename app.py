1117b2cd820cb758e13f865231a7d12f8f9d52b0
Muh alif alghifari <alifalghifari798@gmail.com>
2022-05-19 09:08:04 +0800

    tes model ml

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+from json import load
+from unittest import result
 from flask import (Flask, flash, make_response,
-                   redirect, render_template, request, session, url_for
+                   redirect, render_template, request, session, url_for, jsonify
                    )
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+import numpy as np
+import struct
+from PIL import Image
+import os
+
 app = Flask(__name__)
 app.secret_key = "randomstring"
+
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+# app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+model = load_model('my_model.h5')
 
 
 @app.route("/")
@@ -59,3 +75,65 @@ def logout():
 def get_cookie():
     email = request.cookies.get('email_user')
     return "Email yang tersimpan di cookie adalah " + email
+
+
+# @app.route('/tesmodel', methods=['GET', 'POST'])
+# def tes_model():
+#     # if request.method == "POST":
+#     # images = request.form['img']
+#     img = tf.keras.preprocessing.image.load_img(
+#         'static/images.jpg', target_size=(224, 224))
+#     # print(images)
+#     x = tf.keras.preprocessing.image.img_to_array(img)
+#     x = np.expand_dims(x, axis=0)
+
+#     images = np.vstack([x])
+#     classes = model.predict(images, batch_size=10)
+#     result = np.argmax(classes[0])
+#     return "hasil " + str(classes[0])
+#     # return render_template('sendImage.html')
+
+
+@app.route('/tesmodel', methods=['GET', 'POST'])
+def tes_model():
+    if request.method == "POST":
+        images = request.files['img']
+        # app.logger.info(images.stream.read())
+
+        # # print(images)
+
+        # img = images.stream.read()
+        # print(f'img {type(img)}')
+        # app.logger.info(img.decode('utf-8', 'ignore'))
+        # print(img)
+        # img = str(img, 'UTF-8')
+        # tes = struct.unpack('<HH', img)
+        # with open(img, encoding="utf8", errors='ignore') as f:
+        #     print(f.read())
+        # img = tf.keras.preprocessing.image.array_to_img(
+        #     img.decode('utf-8', 'ignore'))
+
+        # save file
+        if images.filename != '':
+            images.save(os.path.join(
+                app.config['UPLOAD_PATH'], images.filename))
+
+        img = tf.keras.preprocessing.image.load_img(
+            os.path.join(
+                app.config['UPLOAD_PATH'], images.filename), target_size=(224, 224))
+        x = tf.keras.preprocessing.image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+
+        img = np.vstack([x])
+        classes = model.predict(img, batch_size=10)
+        result = np.argmax(classes[0])
+
+        # menghapus file
+        os.remove(os.path.join(
+            app.config['UPLOAD_PATH'], images.filename))
+        return 'hasil training : ' + str(classes) + ' hasil akhir : ' + str(result)
+    return render_template('sendImage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
